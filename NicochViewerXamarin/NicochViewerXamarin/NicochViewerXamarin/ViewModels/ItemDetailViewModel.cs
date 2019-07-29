537a98dcc86f3c2ab5ebbf56d6c8e364d74c2dfd
kurema <kurema_makoto_software@yahoo.co.jp>
2019-07-29 11:41:17 +0900

    init
    結局開発中止？

---
+++ NicochViewerXamarin/NicochViewerXamarin/NicochViewerXamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NicochViewerXamarin.Models;
+
+namespace NicochViewerXamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
